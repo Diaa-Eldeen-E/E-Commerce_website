0c7bd16198ad0b778e82b62b7b1e486ec3d55261
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2022-05-29 09:49:25 +0200

    Add admin layout

---
+++ src/layouts/AdminLayout.js
@@ -0,0 +1,31 @@
+import {Outlet} from "react-router-dom";
+import React from "react";
+import Navigationbar from "../components/Admin/Navigationbar";
+
+
+const Layout = function () {
+
+    return (
+
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <Navigationbar/>
+                </div>
+
+                <div className="row">
+
+                    <Outlet/>
+
+                </div>
+
+            </div>
+        </div>
+
+    );
+
+
+}
+
+
+export default Layout;
