75f67dcab337f565cb01b14c3cc1f0259ab21e2e
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2022-06-29 11:30:26 +0200

    Add custom validation rule for generic-name

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Scout\Builder;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,21 @@ class AppServiceProvider extends ServiceProvider
     {
         //
 
+        //Add this custom validation rule.
+        Validator::extend('generic_name', function ($attribute, $value) {
+
+            // This will only accept alpha, numeric, spaces, hyphens, and underscores.
+            // match alphanumeric with \w
+            // match hyphen with \-
+            // match underscore with \_
+            // match space with \s
+            // the + indicates that there must be at least one character for it to match. use a * instead, if
+            // a zero-length string is also ok
+            return preg_match('/^[\w\-\_\s]+$/', $value);
+
+        });
+
+        // Add this custom method to scout search
         Builder::macro('count', function () {
             return $this->engine()->getTotalCount(
                 $this->engine()->search($this)
--- backend/resources/lang/en/validation.php
+++ backend/resources/lang/en/validation.php
@@ -129,6 +129,7 @@ return [
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute must be a valid URL.',
     'uuid' => 'The :attribute must be a valid UUID.',
+    'generic_name' => 'The :attribute must only contain letters, numbers, spaces, dashes and underscores.',
 
     /*
     |--------------------------------------------------------------------------
